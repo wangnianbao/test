637509e8a80bf874b2ced7737285b01525ee01c2
wnb <1910975064@qq.com>
2016-07-14 23:00:23 +0800

    comit second submit

--- wnb_web/src/main/java/com/wnb/Second.java
+++ wnb_web/src/main/java/com/wnb/Second.java
@@ -7,5 +7,7 @@ public class Second {
     public static void main(String[] args) {
         System.out.println("我是王年宝");
         System.out.println("我是王金生");
+        System.out.println("我是王金生");
+        System.out.println("我是王金生");
     }
 }
