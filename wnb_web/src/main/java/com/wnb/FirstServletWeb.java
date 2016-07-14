756d9fffeb5edfb9b7805dcf540b9cd322e4810f
wnb <1910975064@qq.com>
2016-07-14 22:23:40 +0800

    wrote a readme file

---
+++ wnb_web/src/main/java/com/wnb/FirstServletWeb.java
@@ -0,0 +1,8 @@
+package com.wnb;
+
+/**
+ * Created by wangnianbao on 2016/7/14.
+ */
+public class FirstServletWeb {
+
+}
---
+++ wnb_web/src/test/java/com/Wnb.java
@@ -0,0 +1,8 @@
+package com;
+
+/**
+ * Created by wangnianbao on 2016/7/14.
+ */
+public class Wnb {
+    
+}
